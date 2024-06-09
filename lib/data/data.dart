999b70719d6dd71a1c1e42cab100405abb32e8f7
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-06-10 00:07:58 +0545

    Data Updated

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -1,48 +1,34 @@
-// ! 3rd Commit
-
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 List<Map<String, dynamic>> transactionsData = [
   {
-    'icon': const FaIcon(
-      FontAwesomeIcons.burger,
-      color: Colors.white,
-    ),
+    'icon': const FaIcon(FontAwesomeIcons.burger, color: Colors.white),
     'color': Colors.yellow[700],
     'name': 'Food',
-    'totalAmount': '-Rs.150',
+    'totalAmount': '-Rs.1245.00',
     'date': 'Today',
   },
   {
-    'icon': const FaIcon(
-      FontAwesomeIcons.bagShopping,
-      color: Colors.white,
-    ),
-    'color': Colors.purple[700],
+    'icon': const FaIcon(FontAwesomeIcons.bagShopping, color: Colors.white),
+    'color': Colors.purple,
     'name': 'Shopping',
-    'totalAmount': '-Rs.1000',
+    'totalAmount': '-Rs.1230.00',
     'date': 'Today',
   },
   {
-    'icon': const FaIcon(
-      FontAwesomeIcons.heartCircleCheck,
-      color: Colors.white,
-    ),
-    'color': Colors.green[700],
+    'icon':
+        const FaIcon(FontAwesomeIcons.heartCircleCheck, color: Colors.white),
+    'color': Colors.green,
     'name': 'Health',
-    'totalAmount': '-Rs.1150',
-    'date': 'Today',
+    'totalAmount': '-Rs.579.00',
+    'date': 'Yesterday',
   },
   {
-    'icon': const FaIcon(
-      FontAwesomeIcons.plane,
-      color: Colors.white,
-    ),
-    'color': Colors.blue[700],
+    'icon': const FaIcon(FontAwesomeIcons.plane, color: Colors.white),
+    'color': Colors.blue,
     'name': 'Travel',
-    'totalAmount': '-Rs.2000',
-    'date': 'Today',
-  },
+    'totalAmount': '-Rs. 350.00',
+    'date': 'Yesterday',
+  }
 ];
