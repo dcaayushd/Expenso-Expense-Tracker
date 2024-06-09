2eb373db6f5d9149bdb7d4caac7bf733c8d4c609
Aayush D.C Dangi <aayushdc123@yahoo.com>
2024-06-10 00:08:45 +0545

    StatScreen Updated

--- lib/screens/stats/chart.dart
+++ lib/screens/stats/chart.dart
@@ -1,7 +1,5 @@
 import 'dart:math';
-
 import 'package:fl_chart/fl_chart.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyChart extends StatefulWidget {
@@ -45,28 +43,20 @@ class _MyChartState extends State<MyChart> {
         switch (i) {
           case 0:
             return makeGroupData(0, 2);
-
           case 1:
             return makeGroupData(1, 3);
-
           case 2:
             return makeGroupData(2, 2);
-
           case 3:
             return makeGroupData(3, 4.5);
-
           case 4:
             return makeGroupData(4, 3.8);
-
           case 5:
             return makeGroupData(5, 1.5);
-
           case 6:
             return makeGroupData(6, 4);
-
           case 7:
             return makeGroupData(7, 3.8);
-
           default:
             return throw Error();
         }
@@ -76,28 +66,20 @@ class _MyChartState extends State<MyChart> {
     return BarChartData(
       titlesData: FlTitlesData(
         show: true,
-        rightTitles: const AxisTitles(
-          sideTitles: SideTitles(
-            showTitles: false,
-          ),
-        ),
-        topTitles: const AxisTitles(
-          sideTitles: SideTitles(
-            showTitles: false,
-          ),
-        ),
+        rightTitles:
+            const AxisTitles(sideTitles: SideTitles(showTitles: false)),
+        topTitles: const AxisTitles(sideTitles: SideTitles(showTitles: false)),
         bottomTitles: AxisTitles(
-          sideTitles: SideTitles(
-            showTitles: true,
-            reservedSize: 38,
-            getTitlesWidget: getTiles,
-          ),
-        ),
+            sideTitles: SideTitles(
+          showTitles: true,
+          reservedSize: 38,
+          getTitlesWidget: getTiles,
+        )),
         leftTitles: AxisTitles(
           sideTitles: SideTitles(
             showTitles: true,
             reservedSize: 38,
-            getTitlesWidget: leftTiles,
+            getTitlesWidget: leftTitles,
           ),
         ),
       ),
@@ -117,58 +99,31 @@ class _MyChartState extends State<MyChart> {
 
     switch (value.toInt()) {
       case 0:
-        text = const Text(
-          '01',
-          style: style,
-        );
+        text = const Text('01', style: style);
         break;
       case 1:
-        text = const Text(
-          '02',
-          style: style,
-        );
+        text = const Text('02', style: style);
         break;
       case 2:
-        text = const Text(
-          '03',
-          style: style,
-        );
+        text = const Text('03', style: style);
         break;
       case 3:
-        text = const Text(
-          '04',
-          style: style,
-        );
+        text = const Text('04', style: style);
         break;
       case 4:
-        text = const Text(
-          '05',
-          style: style,
-        );
+        text = const Text('05', style: style);
         break;
       case 5:
-        text = const Text(
-          '06',
-          style: style,
-        );
+        text = const Text('06', style: style);
         break;
       case 6:
-        text = const Text(
-          '07',
-          style: style,
-        );
+        text = const Text('07', style: style);
         break;
       case 7:
-        text = const Text(
-          '08',
-          style: style,
-        );
+        text = const Text('08', style: style);
         break;
       default:
-        text = const Text(
-          '',
-          style: style,
-        );
+        text = const Text('', style: style);
         break;
     }
     return SideTitleWidget(
@@ -178,28 +133,26 @@ class _MyChartState extends State<MyChart> {
     );
   }
 
-  Widget leftTiles(double value, TitleMeta meta) {
+  Widget leftTitles(double value, TitleMeta meta) {
     const style = TextStyle(
       color: Colors.grey,
       fontWeight: FontWeight.bold,
       fontSize: 14,
     );
     String text;
-
     if (value == 0) {
-      text = ('1k');
+      text = '1K';
     } else if (value == 2) {
-      text = ('2k');
+      text = '2K';
     } else if (value == 3) {
-      text = ('3k');
+      text = '3K';
     } else if (value == 4) {
-      text = ('4k');
+      text = '4K';
     } else if (value == 5) {
-      text = ('5k');
+      text = '5K';
     } else {
       return Container();
     }
-
     return SideTitleWidget(
       axisSide: meta.axisSide,
       space: 0,
--- lib/screens/stats/stats.dart
+++ lib/screens/stats/stats.dart
@@ -1,19 +1,15 @@
-import 'package:expenso_expense_tracker/screens/stats/my_chart.dart';
-import 'package:fl_chart/fl_chart.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class StatsScreen extends StatelessWidget {
-  const StatsScreen({super.key});
+import 'chart.dart';
+
+class StatScreen extends StatelessWidget {
+  const StatScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 25.0,
-          vertical: 10,
-        ),
+        padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -21,22 +17,21 @@ class StatsScreen extends StatelessWidget {
               'Transactions',
               style: TextStyle(
                 fontSize: 20,
-                fontWeight: FontWeight.bold,
+                fontWeight: FontWeight.bold
               ),
             ),
             const SizedBox(height: 20),
             Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(12),
-              ),
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.width,
-              // color: Colors.red,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(12)
+              ),
               child: const Padding(
                 padding: EdgeInsets.fromLTRB(12, 20, 12, 12),
                 child: MyChart(),
-              ),
+              )
             )
           ],
         ),
